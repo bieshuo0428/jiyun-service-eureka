1d3e1942ae0aaa428f32d06f808e6c52486ba030
bieshuo0428 <1024635097@qq.com>
2020-08-04 22:27:42 +0800

    作业3

---
+++ src/main/java/cn/jiyun/JiyunEurekaApplication.java
@@ -0,0 +1,15 @@
+package cn.jiyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer   //声明当前应用是一个EurekaServer
+public class JiyunEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiyunEurekaApplication.class, args);
+    }
+
+}
